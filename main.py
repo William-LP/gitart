d0f8fc40004a94f54faa381e5823e4d0571e8180
William-LP <william.le-pommelet@capgemini.com>
2021-05-11 21:56:02 +0200

    hi there

---
+++ main.py
@@ -0,0 +1,41 @@
+import numpy as np
+from datetime import datetime
+from random_word import RandomWords
+import time
+from python_git_wrapper import Repository
+
+# highlight the '1' to see the pixel art
+git_calendar = np.array([['x', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                         ['x', 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0],
+                         ['x', 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0],
+                         ['x', 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0],
+                         ['x', 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],                                          
+                         ['x', 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0, 0, 0, 0, 0, 0, 0]              
+])
+
+# create a list by column (github calendar format)
+flatten_matrix=git_calendar.flatten('F')
+# remove the 'x' that represent the days out of current year
+calendar = np.delete(flatten_matrix, np.where(flatten_matrix == 'x'))
+# get the day of the year : 1 for January 1st
+day_of_year = datetime.now().timetuple().tm_yday 
+
+# if today one need to mark the calendar with a pixel :
+if calendar[day_of_year] == 1 :
+        for i in range(1,10):
+        # get a random word 
+        a_random_word = RandomWords().get_random_word()
+        # write that word to a file  
+        f = open("hello_world.txt", "w")
+        f.write(a_random_word)
+        f.close()
+        # set this directory as a repo
+        repository = Repository('.')
+        # stage the whole repo for next commit because adding only updated file doesn't work
+        repository.add_files('.')
+        # commit
+        repository.commit(a_random_word)
+        # push
+        repository.push()
+
---
+++ readme.md
@@ -0,0 +1,6 @@
+# gitart
+
+## What is that
+
+The purpose of this code is to do some art with github contributing panel
+
